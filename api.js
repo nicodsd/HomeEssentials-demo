ff947c3ea15c4e808f584c56b55df460ec55ad0d
nicodsd <nilonb7@gmail.com>
2026-06-26 22:52:30 -0300

    fix api rul

--- api.js
+++ api.js
@@ -5,4 +5,12 @@ if (process.env.NODE_ENV === "production") {
     apiUrl = import.meta.env.VITE_API
 }
 
+if (apiUrl && !apiUrl.startsWith('http://') && !apiUrl.startsWith('https://')) {
+    apiUrl = 'https://' + apiUrl
+}
+
+if (apiUrl && !apiUrl.endsWith('/')) {
+    apiUrl += '/'
+}
+
 export default apiUrl
